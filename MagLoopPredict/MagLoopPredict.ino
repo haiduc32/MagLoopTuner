33686fd5591047117afb1bf38aa1dbac4688b98f
Radu Pascal <haiduc32@gmail.com>
2022-02-19 18:10:36 +0200

    working hard..

--- MagLoopPredict/MagLoopPredict.ino
+++ MagLoopPredict/MagLoopPredict.ino
@@ -22,23 +22,36 @@
 #include <TMCStepper.h>
 #include <AccelStepper.h>
 #include <SoftwareSerial.h>
+#include <EEPROM.h>
 
 
 #define EN_PIN           7 // Enable
 #define DIR_PIN          9 // Direction
 #define STEP_PIN         8 // Step
 #define CS_PIN           10 // Chip select
+#define BTN              19 // the button.. (A5 pin)
+
+//just a guess as the maximum number of data points you'll need
+#define MAX_DATA_POINTS_PER_BAND 20
 
 #define MAX_SPEED        40 // In timer value
 #define MIN_SPEED      1000
 
-#define STALL_VALUE      17 // [-64..63]
+#define STALL_VALUE      18 // [-64..63]
 #define R_SENSE 0.11f // R Sense resistor value - can be measured on board (the biggest ones)
                       // Match to your driver
                       // SilentStepStick series use 0.11
                       // UltiMachine Einsy and Archim2 boards use 0.2
                       // Panucatt BSD2660 uses 0.1
                       // Watterott TMC5160 uses 0.075
+
+#define MAGIC 0x4444 // a magic number to write to/read from EEPROM so we know 
+                     // that it's populated and not random data
+
+struct DataPoint {
+  int pos;
+  long freq;
+};
 
 // Select your stepper driver type
 TMC2130Stepper driver(CS_PIN, R_SENSE);                           // Hardware SPI
@@ -53,13 +66,37 @@ using namespace TMC2130_n;
 #define STEP_PORT     PORTF // Match with STEP_PIN
 #define STEP_BIT_POS      0 // Match with STEP_PIN
 
+bool calibration = false;
+int calibPoints = 0;
+DataPoint *calib;
+
 void setup() {
+  pinMode(BTN, INPUT_PULLUP);
+  delay(1);
+  bool btnState = digitalRead(BTN);
+  if (btnState == LOW) {
+    calibration = true;
+  } else {
+    //load all calibration data points
+    int magic;
+    EEPROM.get(0, magic);
+    if (magic == MAGIC) {
+      EEPROM.get(2, calibPoints);
+      calib = malloc(sizeof(DataPoint)*calibPoints);
+      for (int i = 0; i < calibPoints; i++) {
+        EEPROM.get(4+sizeof(DataPoint)*i, calib[i]);
+      }
+    }
+  }
+  
   delay(1000);
   stepper.setEnablePin(EN_PIN);
   SPI.begin();
   Serial.begin(38400);         // Init serial port and set baudrate
   while(!Serial);               // Wait for serial port to connect
   Serial.println("\nStart...");
+
+  if (calibration) Serial.println("Calibration mode");
 
   softSerial.begin(38400);
 
@@ -93,7 +130,7 @@ void setup() {
 ////    driver.coolstep_min_speed(0xFFFFF); // 20bit max
 
     stepper.setMaxSpeed(2000); // 100mm/s @ 80 steps/mm
-    stepper.setAcceleration(1000*120); // 2000mm/s^2
+    stepper.setAcceleration(200*120); // 2000mm/s^2
     stepper.setEnablePin(EN_PIN);
     stepper.setPinsInverted(true, false, true);
     //TODO: test that works ok with removing the next two lines
@@ -141,44 +178,44 @@ void home() {
   }
   stepper.disableOutputs();
 
-  // a desperate attempt to get the homing right
-  // seems to help to get repetitive results
-  delay(10);
-  stepper.enableOutputs();
-  stepper.setCurrentPosition(0);
-  stepper.moveTo(-32);
-  stepper.runToPosition();
-  delay(200);
-
-  delay(10);
-  stepper.enableOutputs();
-  stepper.setCurrentPosition(0);
-  stepper.moveTo(-32);
-  stepper.runToPosition();
-  delay(200);
-
-  delay(10);
-  stepper.enableOutputs();
-  stepper.setCurrentPosition(0);
-  stepper.moveTo(-32);
-  stepper.runToPosition();
-  delay(200);
+//  // a desperate attempt to get the homing right
+//  // seems to help to get repetitive results
+//  delay(10);
+//  stepper.enableOutputs();
+//  stepper.setCurrentPosition(0);
+//  stepper.moveTo(-32);
+//  stepper.runToPosition();
+//  delay(200);
+//
+//  delay(10);
+//  stepper.enableOutputs();
+//  stepper.setCurrentPosition(0);
+//  stepper.moveTo(-32);
+//  stepper.runToPosition();
+//  delay(200);
+//
+//  delay(10);
+//  stepper.enableOutputs();
+//  stepper.setCurrentPosition(0);
+//  stepper.moveTo(-32);
+//  stepper.runToPosition();
+//  delay(200);
 
   stepper.setCurrentPosition(0);
   stepper.disableOutputs();
-}
 
-bool isHoming = true;
+  calibration = true;
+}
 
 long lastFreq = 0;
 void loop() {
-  softSerial.print("FA;");
-  Serial.println("FA;");
-  softSerial.flush();
-  String response = softSerial.readStringUntil(';');
-  Serial.println(response);
-  long freq = response.substring(5).toInt();
-  Serial.println(freq);
+  if (calibration) {
+    calibrate();
+  }
+  
+  long freq = readFreq();
+  
+  
   if (freq == lastFreq) {
     delay(500);
     return;
@@ -193,11 +230,7 @@ void loop() {
 
     //new position
     int pos = 3180 + (int)relative;
-    Serial.print("Move to ");Serial.println(pos, DEC);
-    stepper.enableOutputs();
-    stepper.moveTo(pos);
-    stepper.runToPosition();
-    stepper.disableOutputs();
+    moveTo(pos);
   }
   //7000000 .. 3180
   //7200000 .. 3610
@@ -206,4 +239,85 @@ void loop() {
 
   //not sure how fast can we poll CAT
   delay(500);
+}
+
+void calibrate() {
+  int dp = 0;
+  DataPoint dataPoints[MAX_DATA_POINTS_PER_BAND];
+
+  for (int i = 0;;i+=50) {
+    int pos = i;
+    moveTo(pos);
+
+    //now we wait for the BTN to be pressed (LOW state), 
+    //short for next, and long for save data point, longer for save
+    bool btn = HIGH;
+    do  {
+      delay (100);
+      btn = digitalRead(BTN);
+    } while (btn);
+    
+    unsigned long pressTime = millis();
+    //now wait till the button is depressed (HIGH state)
+    do  {
+      delay (100);
+      btn = digitalRead(BTN);
+    } while (!btn);
+
+    //saving 4 bytes of ram..
+    pressTime = millis() - pressTime;
+
+    if (pressTime < 1000) {
+      //short press - continue to next position
+    } else if (pressTime < 2000) {
+      // add data point
+      Serial.println("Adding data point");
+      dataPoints[dp].pos = i;
+      dataPoints[dp].freq = readFreq();
+    } else {
+      //save band data and reset to 0 position
+      //save total number of data points at address 0
+      for (int j = 0; j < dp; j++) {
+        int addr = 4 + calibPoints * sizeof(DataPoint);
+        EEPROM.put(addr, dataPoints[j]);
+        calibPoints++;
+      }
+
+      //write the calibration points count
+      EEPROM.put(0, MAGIC);
+      EEPROM.put(2, calibPoints);
+
+      //finally, time to reset to position 0
+      moveTo(0);
+      break;
+    }
+  }
+}
+
+void moveTo(int pos) {
+  Serial.print("Move to ");Serial.println(pos, DEC);
+  stepper.enableOutputs();
+  stepper.moveTo(pos);
+  stepper.runToPosition();
+  stepper.disableOutputs();
+}
+
+long readFreq() {
+  softSerial.print("FA;");
+  Serial.println("FA;");
+  softSerial.flush();
+  String response = softSerial.readStringUntil(';');
+  Serial.println(response);
+  long freq = response.substring(5).toInt();
+  Serial.println(freq);
+
+  return freq;
+}
+
+int getBand(unsigned long freq) {
+  if (freq > 6500000 && freq < 7500000) {
+    return 40;
+  } else if (freq > 1300000 && freq < 15000000) { 
+    return 20;
+  }
 }
